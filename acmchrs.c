c3ba195b72088026686d8abb4bce8bf8b28ed808
meraxex <klaus10kx@gmail.com>
2018-10-15 14:07:50 -0700

    removed unnecessary stuff

--- acmchrs.c
+++ acmchrs.c
@@ -5,54 +5,6 @@
 void tohex(char c, char s[]);
 int main(int argc, char const *argv[])
 {
-    /*
-    int ch, ls, val, i, nonascii,
-        counts[256], cnt, tot;
-    char s[3];
-
-    for (i = 0; i < 256; i++)
-        counts[i] = 0;
-
-    tot = 0;
-    while ((ch = getchar()) != EOF) {
-        val = ch - '0' + 48;
-        if (val < 256) {
-            tot++;
-            counts[val]++;
-        }
-        else nonascii++;
-        ls = ch;
-    }
-    tohex(ls, s);
-    printf("total: %d\n", tot);
-    printf("last: %s\n", s);
-
-    cnt = 0;
-    for (i = 0; i < 128; i++)
-        cnt += counts[i];
-
-    printf("non-space control: %d\n", cnt);
-    cnt = counts[32];
-    for (i = 9; i < 14; i++)
-        cnt += counts[i];
-    printf("whitespace: %d\n", cnt);
-    if (counts[9]  > 0) printf("  09 %d\n", counts[9]);
-    if (counts[10] > 0) printf("  0A %d\n", counts[10]);
-    if (counts[11] > 0) printf("  0B %d\n", counts[11]);
-    if (counts[12] > 0) printf("  0C %d\n", counts[12]);
-    if (counts[13] > 0) printf("  0D %d\n", counts[13]);
-    if (counts[32] > 0) printf("  20 %d\n", counts[32]);
-
-    cnt = 0;
-    for (i = 33; i < 127; i++)
-        cnt += counts[i];
-
-    printf("non-space printable: %d\n", cnt);
-    printf("non-ascii: %d\n", nonascii);
-
-    return 0;
-}
-*/
     int ws, nsp, nsc, ncc, del, bs,
         tb, vt, nl, ff, cr, cnt;
     char ch, ls, s[3];
